95a3ba67e9516fc747d499205701211ef68400d3
Shajana9 <shajana.dev@gmail.com>
2026-04-16 13:56:23 -0400

    Update docker-compose and gateway for new services, fix CORS and port

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -6,7 +6,16 @@ const proxyRoutes = require("./routes/proxyRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json());
 app.use(morgan("dev"));
 
