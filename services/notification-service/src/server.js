9b8897b21247ffc3ebdc3af4cd6250e33d25e7c8
vanaiyalini <kyalini2001@gmail.com>
2026-04-17 13:18:01 +0530

    Revert "Merge branch 'patient-module'"
    This reverts commit e5e3b089b0ae29d0724418ee1c626e50c1038016, reversing
    changes made to ae4ee0723919581ae2e50e31c8a6f7d7ee365d8a.

--- services/notification-service/src/server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-
-const PORT = process.env.PORT || 5006;
-
-app.listen(PORT, () => {
-  console.log(`Notification service running on port ${PORT}`);
-});
